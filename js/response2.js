ff7993b7ff24997a60b642482e2a0f1c109bc807
jong lee <lsljw94@daum.net>
2022-08-22 16:36:47 +0900

    nav 애니메이션

--- js/response2.js
+++ js/response2.js
@@ -17,6 +17,22 @@ $(document).ready(function(){
         
     });
     
+    // 스크롤 이벤트를 이용해서
+    // 스크롤바의 탑의 값이 지정한 값보다 크면
+    // 네비를 위에서 아래로 애니메이션하면서 고정
+    // 스크롤바의 탑의 값이 지정한 값이하라면 
+    // 네비를 원상태로 돌려놓는다.
+    $(window).scroll(function(){
+        var sTop=$(window).scrollTop();
+        if(sTop>150){
+            // 위에서 아래로 애니메이션 고정
+            $(".d-nav").addClass("d-nav-add");
+        }else{
+            // 원상태로 복귀
+            $(".d-nav").removeClass("d-nav-add")
+        }
+    });
+
     
 
 });
